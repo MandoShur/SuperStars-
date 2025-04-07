efa190124935ec4d19144317edc263822b8a9fc0
Steven Gomez <145067065+StevenGomez0@users.noreply.github.com>
2025-04-07 15:29:10 -0700

    small code changes
    added some basic comments
    
    made a field for executing stuff after the value of "grounded" changes from false to true
    
    made a generic method to reset dive and double jump cd (will be used variously later)

--- UntilClosing/Assets/Scripts/PlayerController.cs
+++ UntilClosing/Assets/Scripts/PlayerController.cs
@@ -27,7 +27,20 @@ public class PlayerController : MonoBehaviour
     [Header ("Grounded Check")]
     public float playerHeight;
     public LayerMask whatIsGround;
-    bool grounded;
+    private bool _grounded; //pretty much a holder for "grounded" bool value DO NOT EDIT THIS BOOL OUTSIDE OF THE BELOW CODE OR YOU EXPLODE
+    bool grounded
+    {
+        get { return _grounded; }
+        set
+        {
+            if(_grounded == false && value == true) //if grounded value changes to true
+            {
+                Debug.Log("grounded set to true");
+                AirborneCooldownResets();
+            }
+            _grounded = value; //set "_grounded" value to "grounded"
+        }
+    }
 
     [Header ("Assigned Objects")]
     public Transform orientation;
@@ -48,17 +61,17 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround); //ground check
 
         MyInput();
         SpeedControl();
 
-        if (grounded)
+        if (grounded) //dynamic drag. grounded drag
         {
             rb.drag = groundDrag;
             readyToDive = true;
-        }
-        else
+        } 
+        else //airborne drag
         {
             rb.drag = 0;
         }
@@ -80,13 +93,12 @@ public class PlayerController : MonoBehaviour
         if(Input.GetKey(jumpKey) && readyToJump && grounded) //grounded jump check 
         {
             readyToJump = false;
-
             Jump();
 
-            Invoke(nameof(ResetJump), jumpCD);
+            Invoke(nameof(ResetJump), jumpCD); //jumpCD
         }
 
-        /*if(Input.GetKey(diveKey) && readyToDive && !grounded)
+  /*    if(Input.GetKey(diveKey) && readyToDive && !grounded)
         {
             readyToDive = false;
 
@@ -106,7 +118,7 @@ public class PlayerController : MonoBehaviour
             rb.AddForce(moveDir.normalized * moveSpeed * 10f * airMovementMulti, ForceMode.Force);
     }
 
-    private void SpeedControl()
+    private void SpeedControl() //this is most likely going to be heavily changed later.
     {
         Vector3 flatVel = new Vector3 (rb.velocity.x, 0f, rb.velocity.z);
 
@@ -117,14 +129,14 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void Jump()
+    private void Jump() //wow its a jump
     {
         rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z); //cancel any vertical velocity
 
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
     }
 
-    private void ResetJump()
+    private void ResetJump() //after whatever timer used to invoke, jump cd off
     {
         readyToJump = !readyToJump;
     }
@@ -134,5 +146,11 @@ public class PlayerController : MonoBehaviour
         rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z); //cancel any vertical velocity
 
         rb.AddForce((transform.right + transform.up)/2 * diveForce, ForceMode.Impulse); //i think the axis i put should work for a dive direction but im not sure and i cant check while im writing this. 
+    }
+
+    public void AirborneCooldownResets() //generic reset for dive and djump. this will probably be used in a few places so im making a reset for this here.
+    {
+        readyToDoubleJump = true;
+        readyToDive = true;
     }
 }
