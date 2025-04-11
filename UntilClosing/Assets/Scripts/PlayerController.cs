ec36a0219f7624d95488858593b2dafb2d8a7942
Steven Gomez <145067065+StevenGomez0@users.noreply.github.com>
2025-04-11 15:30:35 -0700

    updated gravity, debugging script and beginning implementing dive
    - added more comments in playercontroller
    - uncommented all the dive related stuff and started editing it
    - reworked gravity, made downwards gravity pick up speed and fall faster after falling for a little bit
    - added debugging script for testing, also text for displaying info from said script

--- UntilClosing/Assets/Scripts/PlayerController.cs
+++ UntilClosing/Assets/Scripts/PlayerController.cs
@@ -1,19 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("KeyBinds")] //maybe in settings this'll be changable(eventually)?
+    //unaccessables and things youre not supposed to edit at all DO NOT EDIT THESE OR YOU EXPLODE VIOLENTLY(and code messes up but thats not important)
+    private bool _grounded; 
+    private float baseGravityScale;
+
+    [Header("Key Binds")] //maybe in settings this'll be changable(eventually)?
     public KeyCode jumpKey = KeyCode.Space;
-    //dont use this yet     public KeyCode diveKey = KeyCode.E;
+    public KeyCode diveKey = KeyCode.E;
 
 
     [Header ("Movement")]
     public float moveSpeed;
     public float groundDrag;
     public float airMovementMulti;
-    public float gravityScale;
+
+    [Header ("Gravity Stuff")]
+    public float gravityScale = 8.5f;
+    public float VerticalVelocityThreshold = 1.4f;
+    public float AccelerationRate = 0.2f;
+    private float TerminalVelo = -14f; //this should be in unaccessables but idc!!!
+
+
 
     [Header ("Jumping")]
     public float jumpForce;
@@ -28,11 +40,10 @@ public class PlayerController : MonoBehaviour
     [Header ("Grounded Check")]
     public float playerHeight;
     public LayerMask whatIsGround;
-    private bool _grounded; //pretty much a holder for "grounded" bool value DO NOT EDIT THIS BOOL OUTSIDE OF THE BELOW CODE OR YOU EXPLODE and the code messes up but whatever
     bool grounded
     {
-        get { return _grounded; }
-        set
+        get { return _grounded; } 
+        set //basically just resets double jump and dive cd when youre grounded
         {
             if(_grounded == false && value == true) //if grounded value changes to true
             {
@@ -51,23 +62,25 @@ public class PlayerController : MonoBehaviour
 
     Vector3 moveDir;
 
-    Rigidbody rb;
+    public Rigidbody rb; //this is just public so debugging script can catch it
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
-        rb.useGravity = false; //disables gravity
+        rb.useGravity = false; //disables gravity, using custom gravity
+
+        baseGravityScale = gravityScale; //establishing base gravity
     }
 
     private void Update()
     {
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround); //ground check
+        grounded = Physics.Raycast(transform.position, -transform.up, playerHeight * 0.5f + 0.2f, whatIsGround); //ground check
 
         MyInput();
         //SpeedControl();
 
-        if (grounded) //dynamic drag. grounded drag
+        if (grounded) //dynamic drag system, though it isnt really doing anything atm since both are set to grounddrag (redundant)
         {
             rb.drag = groundDrag;
             readyToDive = true;
@@ -85,10 +98,31 @@ public class PlayerController : MonoBehaviour
         GravityForce();
     }
 
-    private void GravityForce()
+    private void GravityForce() //essentially just gravity handling
     {
-        //custom gravity code, hopefully will fix jumps feeling really floaty on descent 
-        rb.AddForce(Physics.gravity * gravityScale, ForceMode.Acceleration); //ok i think it worked
+        if (rb.velocity.y > VerticalVelocityThreshold && !grounded)
+        {
+            rb.AddForce(Physics.gravity, ForceMode.Acceleration); //regular gravity on ground
+        }
+        else //note to self terminal velo AT 8 gravscale is -14.12639 
+        {
+            rb.AddForce(Physics.gravity * gravityScale, ForceMode.Acceleration); //if y velocity falls below float, increase gravity to make jump less floaty
+            if(rb.velocity.y < TerminalVelo) //if, in increased gravity, terminal velocity is reached (i think -14) begin to increase gravity overtime 
+            {
+                //Debug.Log("coroutine started");
+                StartCoroutine(FallFaster()); //small(kinda redundant) note, the camera gets really zoomed out if you fall for longer than like 2.5 seconds, but if youre falling for that long you probably fell off the map 
+            }
+        }
+        if(rb.velocity.y >= -0.1f) //if gravity is above baseline minus a bit (not falling) reset gravity to original gravity
+        {
+            gravityScale = baseGravityScale;
+        }
+    }
+
+    IEnumerator FallFaster() //wait for 1/10th of a second then add to gravity scale with accel rate every call
+    {
+        yield return new WaitForSeconds(0.1f);
+        gravityScale = AccelerationRate + gravityScale;
     }
 
     void MyInput()
@@ -99,18 +133,17 @@ public class PlayerController : MonoBehaviour
         if(Input.GetKey(jumpKey) && readyToJump && grounded) //grounded jump check 
         {
             readyToJump = false;
-            Jump();
+            Jump(); //jump (wow)
 
             Invoke(nameof(ResetJump), jumpCD); //jumpCD
         }
 
-  /*    if(Input.GetKey(diveKey) && readyToDive && !grounded)
+        if(Input.GetKey(diveKey) && readyToDive && !grounded)
         {
             readyToDive = false;
 
             Dive();
-
-        } whoops not implemented yet :p */
+        }
     }
 
     void MovePlayer()
@@ -121,10 +154,10 @@ public class PlayerController : MonoBehaviour
             rb.AddForce(moveDir.normalized * moveSpeed * 10f, ForceMode.Acceleration);
 
         else if(!grounded)
-            rb.AddForce(moveDir.normalized * moveSpeed * 10f * airMovementMulti, ForceMode.Force); //this is probably gonna become redundant
+            rb.AddForce(moveDir.normalized * moveSpeed * 10f * airMovementMulti, ForceMode.Force); //this is probably gonna become redundant (it has)
     }
 
-    /*private void SpeedControl() //this is most likely going to be heavily changed later.
+    /*private void SpeedControl() //this is most likely going to be heavily changed later. or be redundant.
     {
         Vector3 flatVel = new Vector3 (rb.velocity.x, 0f, rb.velocity.z);
 
@@ -139,10 +172,10 @@ public class PlayerController : MonoBehaviour
     {
         rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z); //cancel any vertical velocity
 
-        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse); //jump
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse); //jump force
     }
 
-    private void ResetJump() //after whatever timer used to invoke, jump cd off
+    private void ResetJump() //after whatever timer/method used to invoke, jump cd off
     {
         readyToJump = !readyToJump;
     }
@@ -151,7 +184,8 @@ public class PlayerController : MonoBehaviour
     {
         rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z); //cancel any vertical velocity
 
-        rb.AddForce((transform.right + transform.up)/2 * diveForce, ForceMode.Impulse); //i think the axis i put should work for a dive direction but im not sure and i cant check while im writing this. 
+        Debug.Log("Dive called");
+        rb.AddForce((transform.forward + (transform.up * 0.5f)).normalized * diveForce, ForceMode.Impulse); //dive; i think the axis i put should work for a dive direction but im not sure and i cant check while im writing this. 
     }
 
     public void AirborneCooldownResets() //generic reset for dive and djump. this will probably be used in a few places so im making a reset for this here.
@@ -160,7 +194,10 @@ public class PlayerController : MonoBehaviour
         readyToDive = true;
     }
 
+
+
     //comment here because im going to forget if i dont, possibly split jump into 2 parts, rising and falling (y.velocity > 0, y.velocity < 0)
     //could give more control over jump curve, and especially over the dumb thing where i either descend super slowly or jump super fast and abruptly stop
     //only problem i see with this is exception cases (which i can manually work with) and just keeping track of it
+    //update to above 4/11, implemented this
 }
---
+++ UntilClosing/Assets/Scripts/ShowSpeed.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ShowSpeed : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public GameObject player;
+    public Rigidbody playerRb;
+    private bool isGravDownwards;
+    public PlayerController playerController;
+    public float lowestYVelo = 0;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (playerRb.velocity.y < playerController.VerticalVelocityThreshold)
+        {
+            isGravDownwards = true;
+        }
+        if (playerRb.velocity.y >= -0.1f)
+        {
+            isGravDownwards = false;
+        }
+        text.text = "Downwards Grav: " + isGravDownwards;
+
+
+        if(playerRb.velocity.y < lowestYVelo)
+        {
+            lowestYVelo = playerRb.velocity.y;
+        }
+    }
+}
